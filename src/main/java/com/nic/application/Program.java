e884f18119985da4e6bf1c801a7c1ad0e1938ec5
Loocien <nicolassilveiracorrea@gmail.com>
2023-11-15 16:54:10 -0300

    Starting to implement Board and Piece

---
+++ src/main/java/com/nic/application/Program.java
@@ -0,0 +1,12 @@
+package com.nic.application;
+
+import com.nic.boardgame.Board;
+import com.nic.boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Board boad = new Board(8, 8);
+
+    }
+}
