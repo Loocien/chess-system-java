e5f829e26b537a1ce64e670bdf41d78d3c3b7d34
Loocien <nicolassilveiracorrea@gmail.com>
2023-11-15 17:15:46 -0300

    Chess layer and printing the board

--- src/main/java/com/nic/application/Program.java
+++ src/main/java/com/nic/application/Program.java
@@ -2,11 +2,13 @@ package com.nic.application;
 
 import com.nic.boardgame.Board;
 import com.nic.boardgame.Position;
+import com.nic.chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
 
-        Board boad = new Board(8, 8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
 
     }
 }
---
+++ src/main/java/com/nic/chess/ChessPiece.java
@@ -0,0 +1,21 @@
+package com.nic.chess;
+
+import com.nic.boardgame.Board;
+import com.nic.boardgame.Piece;
+
+public class ChessPiece extends Piece {
+
+    private Color color;
+
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+
+
+}
