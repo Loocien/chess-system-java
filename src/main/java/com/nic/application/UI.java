b1dc898d5f9ae44a39c403b7a8c7ce7cc5bb62c4
Loocien <nicolassilveiracorrea@gmail.com>
2023-11-18 12:06:16 -0300

    Switching player with turn

--- src/main/java/com/nic/application/UI.java
+++ src/main/java/com/nic/application/UI.java
@@ -1,5 +1,6 @@
 package com.nic.application;
 
+import com.nic.chess.ChessMatch;
 import com.nic.chess.ChessPiece;
 import com.nic.chess.ChessPosition;
 import com.nic.chess.Color;
@@ -45,6 +46,13 @@ public class UI {
         catch (RuntimeException e ) {
             throw new InputMismatchException("Error reading ChessPosition. Valid value are from a1 to h8");
         }
+    }
+
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn : " + chessMatch.getTurn());
+        System.out.println("Wainting player: " + chessMatch.getCurrentPlayer());
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
